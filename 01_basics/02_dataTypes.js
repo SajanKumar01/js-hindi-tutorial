f8d7c7bc53bee2f236466cbc1767e7846f7e78bc
SajanKumar01 <sajanrajak9570@gmail.com>
2023-10-04 10:24:48 +0530

    Data Types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+// !  Data Types  in JavaScript....
+
+// Code readAbility Should be high...
+
+let name = "sajankumar"
+let age = 18;
+let isLoggedIn = true;
+let state = null
+
+/**
+ * 01. Number => 1,2,3.......  ye  .2 to power 53 ( the range of number)
+ * 02. Bigint
+ * 03. string => "sajankumar"
+ * 04. Boolean => true / false
+ * 05. Null => Standalone Value
+ * 06. Undefined =>  Empty
+ * 07. Symbol => unique
+ * 
+ * 
+ * 08. boject
+ */
+
+console.log(typeof "sajan kumar ");
+console.log(typeof age);
+console.log(typeof null);// the output is objects.
+console.log(typeof undefined); // the output is undefined.
+
+
