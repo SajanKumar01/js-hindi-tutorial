ca1cd110d9bb372f486d26103ece3c14c5bd9d18
SajanKumar01 <sajanrajak9570@gmail.com>
2023-10-09 20:08:17 +0530

    functions in js

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -1,72 +1,85 @@
 function sayMyname() {
-    console.log("s");
-    console.log("a");
-    console.log("j");
-    console.log("a");
-    console.log("n");
+  console.log("s");
+  console.log("a");
+  console.log("j");
+  console.log("a");
+  console.log("n");
 }
 
 // sayMyname()
 
-
 /*function AddTwoNumeber(num1, num2) {
    console.log(num1 + num2);
 }
-AddTwoNumeber(10 ,58)*/
-
+AddTwoNumeber(10, 58)*/
 
 function addNumbers(number1, number2) {
-    let result = number1 + number2
-    return result
-    //    return number1 + number2
+  let result = number1 + number2;
+  return result;
+  //    return number1 + number2
 }
 
-const result = addNumbers(8, 5)
-// console.log(result);
-
-
+const result = addNumbers(8, 5);
+// console.log("result", result );
 
 function loginUserName(username) {
-    return `${username} just logged in`
+  return `${username} just logged in`;
 }
 // console.log(loginUserName(" sajan kuamr"));
-
-
-
 
 function loginUserName(username) {
   if (username === undefined) {
     console.log("please enter a username");
-    return
+    return;
   }
-    return `${username} just logged in`
+  return `${username} just logged in`;
 }
 
 // console.log(loginUserName());
 
-
-
-
-
-function loginUserName(username) {
-    if (!username) {
-      console.log("please enter a username");
-      return
-    }
-      return `${username} just logged in`
+function loginUserName(username = "sajan") {
+  if (!username) {
+    console.log("please enter a username");
+    return;
   }
-  
-//   console.log(loginUserName());
-  
-
-
-// Shopping cart 
-
-function calculatCartPrece(...num1) {
-    return num1
+  return `${username} just logged in`;
 }
 
-console.log(calculatCartPrece(200, 300, 500,890));
+// console.log(loginUserName());
+
+// Shopping cart
+
+function calculateCartPrice(val1, val2, ...num1) {
+  return num1;
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
 
 
+const user = {
+  username: "hitesh",
+  price: 199
+}
 
+function handleObject(anyobject){
+  // console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+
+
+//********* 
+handleObject({
+  username: "sam",
+  price: 399
+})
+
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+  return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
