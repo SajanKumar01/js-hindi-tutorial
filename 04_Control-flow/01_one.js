14bf7ea418ee19a426201026ac3a96faff9a5588
SajanKumar01 <sajanrajak9570@gmail.com>
2023-10-11 18:36:41 +0530

    if condations

---
+++ 04_Control-flow/01_one.js
@@ -0,0 +1,64 @@
+//  ! condinional
+
+// ?  if
+
+// const isuserLoggedIn = true;
+// const temperature = 46;
+
+// if (temperature < 50) {
+//   console.log("less then 50");
+// } else {
+//   console.log("Temperature is grater then 50");
+// }
+
+
+// ******************
+
+// const score = 200
+
+// if (score > 100) {
+//     const power ="fly"
+//     console.log(`User power: ${power}`);
+// }
+// console.log(`User power: ${power}`);
+
+
+// **************
+
+/*let balence = 10000
+
+if (balence < 500 ) {
+    console.log("less then ");
+}else if(balence < 750 ){
+    console.log("less then 750");
+}else if (balence < 900) {
+    console.log("less then 900 ");
+}else{
+    console.log("less then 1200");
+}*/
+
+// **********
+
+const userloggedIn = true
+const debitCard = true
+
+const loggedInGoogle = false
+const loggedInEmail = true
+
+if (userloggedIn && debitCard ) {
+    console.log("Allow to Buy Course");
+}
+
+if (loggedInGoogle || loggedInEmail) {
+    console.log("User loggedIn to our website");
+}
+/**
+ *  1. < less Then
+ *  2. > Greater then
+ *  3. <=
+ *  4. >=
+ *  5. ==  equal hai kiya
+ *  6. !=
+ *  7. ===
+ *  8. !==
+ */
---
+++ 04_Control-flow/02_Two.js
@@ -0,0 +1,59 @@
+// ! Switch case
+
+/*  switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+} */
+
+const months = 3;
+switch (months) {
+  case 1:
+    console.log("january");
+    break;
+
+  case 2:
+    console.log("feb");
+    break;
+
+  case 3:
+    console.log("march");
+    break;
+
+  case 4:
+    console.log("April");
+    break;
+
+  case 5:
+    console.log("may");
+    break;
+  case 6:
+    console.log("june");
+    break;
+  case 7:
+    console.log("july");
+    break;
+  case 8:
+    console.log("Augest");
+    break;
+
+  case 9:
+    console.log("September");
+    break;
+
+  case 10:
+    console.log("octover");
+    break;
+  case 11:
+    console.log("Nomber");
+    break;
+  case 12:
+    console.log("December");
+    break;
+  default:
+    console.log("Default case match");
+    break;
+}
