5652e8f408f94f1df9275a722fe3fce815664113
SajanKumar01 <sajanrajak9570@gmail.com>
2023-10-11 21:48:08 +0530

    while  loop

---
+++ 05_iterations/02_Two.js
@@ -0,0 +1,24 @@
+// while  loop
+
+let s = 0
+while (s <= 10) {
+    // console.log(`values of index is ${s}`);
+    s = s +2
+}
+
+
+let myArray = ["flash", "batman", "supermen"]
+let arr = 0
+while (arr < myArray.length) {
+    // console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+
+// ? do while loop 
+let score = 11
+do{
+  console.log(`saore is ${score}`);
+  score++
+} while (score <= 10);
+    
