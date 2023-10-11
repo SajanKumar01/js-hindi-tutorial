4e052154670ce4937274eeb1c59ac7565da73504
SajanKumar01 <sajanrajak9570@gmail.com>
2023-10-11 21:53:26 +0530

    for in loop.

---
+++ 05_iterations/04_Four.js
@@ -0,0 +1,34 @@
+// for in
+
+const myobject = {
+    js:'javascript',
+    cpp:'c++',
+    py:'python',
+    rb:'rubi'
+}
+for (const key in myobject) {
+    // console.log(myobject[key]);
+    // console.log(`${key} shortcut is for ${myobject[key]}`);
+}
+
+
+const programming = ["js", "rb", "py","java" ,"php"]
+
+for (const key in programming) {
+    // console.log(programming[key]);
+}
+
+
+
+
+
+// const map = new Map()
+// map.set("IN" ,"India")
+// map.set("USA" ,"united State of America")
+// map.set("Fr" ,"France")
+// map.set("jpn" ,"japan")
+
+// // console.log(map);
+// for (const [key, value] in map) {
+//     console.log(key, ':-', value);
+// }
