f118d630e4740cbbbcf0fa39f7aa59910eee5771
SajanKumar01 <sajanrajak9570@gmail.com>
2023-10-14 18:33:05 +0530

    Background Color Switcher

---
+++ 07_projects/01_projects/script.js
@@ -0,0 +1,28 @@
+// ! JavaScript Background Color Switcher
+
+const button = document.querySelectorAll('.buttons')
+const body = document.querySelector('body')
+
+button.forEach(function(button){
+    // console.log(button);
+    button.addEventListener('click',function(e){
+      console.log(e);
+      console.log(e.target);
+
+      if (e.target.id === 'grey') {
+        body.style.backgroundColor = e.target.id
+      }
+
+      if (e.target.id === 'white') {
+        body.style.backgroundColor = e.target.id
+      }
+
+      if (e.target.id === 'blue') {
+        body.style.backgroundColor = e.target.id
+      }
+
+      if (e.target.id === 'yellow') {
+        body.style.backgroundColor = e.target.id
+      }
+    })
+});
